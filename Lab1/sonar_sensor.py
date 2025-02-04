503107735bf518ae328af0263a475d688b6ec369
PopoLleKok0 <Mouadbenlahbibca@gmail.com>
2025-02-04 12:56:06 -0500

    corrected sonor

--- Lab1/sonar_sensor.py
+++ Lab1/sonar_sensor.py
@@ -1,29 +1,17 @@
-import time
-import threading
 from HCSR04 import HCSR04
-
-# Number of samples for averaging distance measurements
 samples = 5
-
-# Creation of sonar sensor instance with trigger pin 7 and echo pin 12
+#creation of sonar sensor
 sensor = HCSR04(7, 12)
 
+# Function for sonar sensor takes HCSR04 object and sample number for accuracy of distance
 def Sonar(sensor, samples):
-    """
-    Function to continuously measure and print distance using the sonar sensor.
-    :param sensor: HCSR04 sensor object
-    :param samples: Number of samples for accuracy
-    """
     while True:
-        s = time.time()  # Start time
-        distance = sensor.measure(samples, "cm")  # Measure distance
-        e = time.time()  # End time
-        
+        s = time.time()
+        distance = sensor.measure(samples, "cm")
+        e = time.time()
         print("Distance:", distance, "cm")
         print("Used time:", (e - s), "seconds")
-        
-        time.sleep(0.01)  # Small delay to prevent excessive CPU usage
+        time.sleep(0.01)
 
-# Create and start a separate thread for the sonar function
 sensorThread = threading.Thread(target=Sonar, args=(sensor, samples))
 sensorThread.start()
